76b4874374e96e7cf5a9773dcd4f12210c1f4c41
tanik12 <koyu.tani01@g.softbank.co.jp>
2020-01-04 00:24:57 +0900

    [feat] 指定したディレクトリ内にあるアノテーションデータ全ての情報を取得できるようにした

--- get_img.py
+++ get_img.py
@@ -1,29 +1,33 @@
 import xml.etree.ElementTree as ET
+import os
 
-def read_xml(xml_file): 
-    img_path = tree.find("path").text
-    root = tree.findall("object")
-    print("写真内にあるオブジェクトの数: ", len(root))
-    count = 0
-    for item in root:
-        count += 1
-        print("===========================")
-        print("=== " + str(count) + "個目のobjectの情報 ===")
-        print(item.find("name").text)
-        bdbox_info = item.findall("bndbox")
-        for bd in bdbox_info:
-            print(bd.find("xmin").text)
-            print(bd.find("ymin").text)
-            print(bd.find("xmax").text)
-            print(bd.find("ymax").text)
-    print(img_path)
+def read_xml(xml_path):
+    xml_files = os.listdir(xml_path)
+    print("画像の合計枚数: ", len(xml_files))
+    for file_name in xml_files:
+        # xml fileを解析
+        tree = ET.parse(xml_path + file_name)
+        img_path = tree.find("path").text
+        objects = tree.findall("object")
+        #print("写真内にあるオブジェクトの数: ", len(objects), ", 画像path: ", img_path) #Debug用
+        for obj in objects:
+            #print("===========================")                  #Debug用
+            #print("=== " + str(count) + "個目のobjectの情報 ===") #Debug用
+            #print(obj.find("name").text)                          #Debug用
 
+            #オブジェクトごとのラベル名とバウンディングボックス情報を取得する。
+            bdbox_info = obj.findall("bndbox")
+            bd_list = []
+            for bd in bdbox_info:
+                xmin = bd.find("xmin").text
+                ymin = bd.find("ymin").text
+                xmax = bd.find("xmax").text
+                ymax = bd.find("ymax").text
+                bd_list += [xmin, ymin, xmax, ymax]
+                #print("===========") #Debug用
+                #print("object_name: ", obj.find("name").text, ", バウンディングボックス: ", bd_list) #Debug用
 
 if __name__ == "__main__":    
-    # xml fileがあるpath
-    xml_path = "/home/tani/git/coco_analytics/VOCdevkit/VOC2012/Annotations/"
-    file_name = "COCO_train2014_000000000061.xml"
-
-    # xml fileを解析
-    tree = ET.parse(xml_path + file_name)
-    read_xml(tree) 
+    # xml fileがあるディレクトリのパス
+    xml_path = "/home/tani/git/traffic_light_dataset/Anotations/"
+    read_xml(xml_path) 
